b735f458c6be316295f7e1bd8e8f6c7751ebdb51
hollyjoc <holly.jo.robertson22@gmail.com>
2017-10-25 18:55:20 -0400

    Shit

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20171025205122) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "plans", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "the_plans", force: :cascade do |t|
     t.string "plan_name"
     t.decimal "plan_price"
