6c6f8a3e4c5fe258467137ada9babc5ef0a0c78b
hollyjoc <holly.jo.robertson22@gmail.com>
2017-10-25 18:30:48 -0400

    trying

--- db/migrate/20171025204602_remote_plan_id_from_user.rb
+++
@@ -1,6 +0,0 @@
-class RemotePlanIdFromUser < ActiveRecord::Migration[5.1]
-  def change
-  	remove_column :user, :plan_id
-  end
-end
-
--- db/migrate/20171025205122_add_plan_id.rb
+++
@@ -1,5 +0,0 @@
-class AddPlanId < ActiveRecord::Migration[5.1]
-  def change
-  	add_column :user, :plan_id, :integer
-  end
-end
