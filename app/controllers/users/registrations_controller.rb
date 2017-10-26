a8c4c2d134bbec7f03c0b4836eb0a2d05ce2a6ab
hollyjoc <holly.jo.robertson22@gmail.com>
2017-10-25 22:41:08 -0400

    Fixed typos in application controller

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,13 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+	def create
+		super do |resource| #Overriding create in RegistrationsController
+			if params[:the_plan]
+				resource.plan.id == params[:the_plan]
+			if resource.plan.id == 2
+				resource.save_with_payment
+			else
+				resource.save
+			end
+		end
+	end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171025205122) do
+ActiveRecord::Schema.define(version: 20171026023317) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20171025205122) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "plans", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "the_plans", force: :cascade do |t|
     t.string "plan_name"
@@ -42,6 +48,7 @@ ActiveRecord::Schema.define(version: 20171025205122) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "plan_id"
+    t.string "stripe_customer_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
